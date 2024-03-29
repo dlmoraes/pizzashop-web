c8ab20331c773e4dcfa0b37d37296cecf9501d6a
Diego Moraes <diegofrost96@gmail>
2024-03-29 19:13:18 -0300

    feat: receita no período

---
+++ src/api/get-daily-revenue-in-period.ts
@@ -0,0 +1,27 @@
+import { api } from '@/lib/axios'
+
+export interface GetDailyRevenuePeriodQuery {
+  from?: Date
+  to?: Date
+}
+
+export type GetDailyRevenueInPeriodResponse = {
+  date: string
+  receipt: number
+}[]
+
+export async function getDailyRevenueInPeriod({
+  from,
+  to,
+}: GetDailyRevenuePeriodQuery) {
+  const response = await api.get<GetDailyRevenueInPeriodResponse>(
+    '/metrics/daily-receipt-in-period',
+    {
+      params: {
+        from,
+        to,
+      },
+    },
+  )
+  return response.data
+}
