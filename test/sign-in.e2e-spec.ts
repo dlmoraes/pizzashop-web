d3e519fd5dfd25e270eca6386b5d770c3805ef72
Diego Moraes <diegofrost96@gmail>
2024-04-15 10:16:00 -0300

    ajustar nomes dos testes em sign-in.e2e.spec

--- test/sign-in.e2e-spec.ts
+++ test/sign-in.e2e-spec.ts
@@ -1,12 +1,14 @@
 import { expect, test } from '@playwright/test'
 
-test('sign in successfull', async ({ page }) => {
+test('sign in successful', async ({ page }) => {
   await page.goto('/sign-in', { waitUntil: 'networkidle' })
 
   await page.getByLabel('Seu e-mail').fill('johndoe@example.com')
   await page.getByRole('button', { name: 'Acessar painel' }).click()
 
-  const toast = page.getByText('Enviamos um link de autenticação para o seu e-mail.')
+  const toast = page.getByText(
+    'Enviamos um link de autenticação para o seu e-mail.',
+  )
 
   expect(toast).toBeVisible()
 
@@ -26,12 +28,10 @@ test('sign in with wrong credentials', async ({ page }) => {
   await page.waitForTimeout(2000)
 })
 
-test('navigate to new restaurantp page', async ({ page }) => {
-  await page.goto('/sign-in', {  waitUntil: 'networkidle'})
+test('navigate to new restaurant page', async ({ page }) => {
+  await page.goto('/sign-in', { waitUntil: 'networkidle' })
 
   await page.getByRole('link', { name: 'Novo estabelecimento' }).click()
 
   expect(page.url()).toContain('/sign-up')
-}) 
-
-
+})
